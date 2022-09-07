a4f155ac58f93c240ed7c941eff0385324b826e3
SulaihaShahema <110550461+SulaihaShahema@users.noreply.github.com>
2022-09-07 12:28:25 +0530

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,8 @@
+//Program to print + ++ +++ using cout
+#import<iostream>
+using namespace std;
+int main(){
+	cout<<"+ \n++ \n+++";
+	return 0;
+}
+
---
+++ Assignment1b.cpp
@@ -0,0 +1,8 @@
+// Program to print your name and age
+#import<iostream>
+using namespace std;
+int main(){
+	cout<<"Your name: Sulaiha Shahema Ansari";
+	cout<<"\nYour age: 23";
+	return 0;
+}
